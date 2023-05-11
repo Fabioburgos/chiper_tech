53aa3e6c7a507564adca110a28a8baa268a612ba
cptburgos <fabucand@hotmail.com>
2023-05-10 23:33:03 -0500

    Modify readme and put comments to the code

--- data.py
+++ data.py
@@ -1,21 +1,38 @@
+from constants import DATABASE
 import pandas as pd
 import os
-from constants import DATABASE
 
+
+# get the list of files from the database directory
 files = os.listdir(DATABASE)
 
-def readDatabase(file = files) -> pd.DataFrame:
-    
+
+def readDatabase(file=files) -> pd.DataFrame:
+    """Reads sales data from the database directory.
+
+    Args:
+        file (list): A list of file names to read data from. Defaults to files in the database directory.
+
+    Returns:
+        pd.DataFrame: A pandas DataFrame containing the sales data.
+    """
+    # loop over each file in the directory
     for file in files:
         
+        # read the data from the file using pandas
         df = pd.read_csv(os.path.join(DATABASE, file), sep="\t", on_bad_lines="skip")
-        df.columns=['salesid', 'listid', 'sellerid', 'buyerid', 'eventid', 'dateid', 'qtysold', 'pricepaid', 'commission', 'saletime']
-        df['saletime'] = pd.to_datetime(df['saletime'])
-        df = df.rename(columns = {'qtysold' : 'y',
-                         'saletime' : 'ds'})
-        df.set_index('ds', inplace=True)
-        df = df.drop(['salesid', 'listid', 'sellerid', 'buyerid', 'eventid', 'dateid', 'pricepaid', 'commission'], axis=1)
         
+        # rename the columns and set index to sale time
+        df.columns=['salesid', 'listid', 'sellerid', 'buyerid', 'eventid', 'dateid',
+                    'qtysold', 'pricepaid', 'commission', 'saletime']
+        df['saletime'] = pd.to_datetime(df['saletime'])
+        df = df.rename(columns={'qtysold': 'y', 'saletime': 'ds'})
+        df.set_index('ds', inplace=True)
+        
+        # drop unnecessary columns and sort by index
+        df = df.drop(['salesid', 'listid', 'sellerid', 'buyerid', 'eventid', 'dateid',
+                      'pricepaid', 'commission'], axis=1)
         df = df.sort_index(ascending=True)
         
+        # return the final DataFrame
         return df
--- main.py
+++ main.py
@@ -1,31 +1,49 @@
 from fastapi import FastAPI
 from data import readDatabase
-from sklearn.linear_model import LinearRegression
-import numpy as np
 from pydantic import BaseModel
 from mlforecast import MLForecast
+from sklearn.linear_model import LinearRegression
+
+import numpy as np
+
 
 class Dayprediction(BaseModel):
+    """Pydantic model representing the request body for the prediction endpoint."""
     day : int
 
 app = FastAPI()
 
 @app.post('/prediction')
 def predictionDays(day : Dayprediction):
+    """
+    Endpoint for predicting sales for a given day using machine learning.
 
+    Args:
+        day (Dayprediction): The input day to predict sales for.
+
+    Returns:
+        A dictionary containing the predicted day and sales.
+    """
+    # Read training data and add unique identifier.
     train = readDatabase().reset_index()[['ds', 'y']]
     train["unique_id"] = np.repeat(0, train.shape[0])
     
+    # Initialize linear regression model.
     model = LinearRegression(fit_intercept=False)
     
+    # Initialize MLForecast object with model, frequency, and lags.
     forecast = MLForecast(
         models = model,
         freq = "D",
         lags = [7, 14, 30]
     )
     
+    # Fit model to training data.
     forecast.fit(train)
+    
+    # Predict sales for the input day.
     prediction = forecast.predict(day.day)
     
+    # Return a dictionary containing the predicted day and sales.
     return {"Day predicted" : f"{prediction['ds']}",
-            "sale prediction" : f"{prediction['LinearRegression']}"}
+            "Sale prediction" : f"{prediction['LinearRegression']}"}
